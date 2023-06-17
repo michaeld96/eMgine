fcc389e3fa001d0f99f9124b97ea908fdf1aa3c1
Michael Dick <michaeldick000@gmail.com>
2023-06-17 10:43:56 -0400

    made basic template for engine

---
+++ README.md
@@ -0,0 +1,8 @@
+# Raycaster Game Engine
+
+## TODO
+- [ ] Create a window function.
+    
+    * This is going to be done with OpenGL.
+- [ ] Create a player draw function.
+- [ ] Create a world draw function.
---
+++ build/CMakeFiles/raycaster.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raycaster.dir/lib/glad/src/glad.c.o"
+  "CMakeFiles/raycaster.dir/lib/glad/src/glad.c.o.d"
+  "CMakeFiles/raycaster.dir/src/main.cpp.o"
+  "CMakeFiles/raycaster.dir/src/main.cpp.o.d"
+  "raycaster"
+  "raycaster.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/raycaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
